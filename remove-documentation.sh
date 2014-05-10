f7dd3a8925eeaa54ff8ef08003dcddb5891fc43b
Ahmet Sezgin Duran <marjinalist1@gmail.com>
2014-05-10 19:10:43 +0300

    Fix error in documentation remover

--- remove-documentation.sh
+++ remove-documentation.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-sudo bash -c 'cat >~/etc/dpkg/dpkg.cfg.d/01_nodoc <<EOL
-path-exclude /usr/share/doc/*
-path-exclude /usr/share/man/*
-path-exclude /usr/share/groff/*
-path-exclude /usr/share/info/*
-path-exclude /usr/share/lintian/*
-path-exclude /usr/share/linda/*
-EOL'
+sudo touch /etc/dpkg/dpkg.cfg.d/01_nodoc
+sudo bash -c "echo 'path-exclude /usr/share/doc/*' >> /etc/dpkg/dpkg.cfg.d/01_nodoc"
+sudo bash -c "echo 'path-exclude /usr/share/man/*' >> /etc/dpkg/dpkg.cfg.d/01_nodoc"
+sudo bash -c "echo 'path-exclude /usr/share/groff/*' >> /etc/dpkg/dpkg.cfg.d/01_nodoc"
+sudo bash -c "echo 'path-exclude /usr/share/info/*' >> /etc/dpkg/dpkg.cfg.d/01_nodoc"
+sudo bash -c "echo 'path-exclude /usr/share/lintian/*' >> /etc/dpkg/dpkg.cfg.d/01_nodoc"
 
 sudo rm -rf /usr/share/doc /usr/share/man /usr/share/groff /usr/share/info /usr/share/lintian
