6d09b9e7e8440d6bdc73be28749acc650b4bc1e2
Ahmet Sezgin Duran <marjinalist1@gmail.com>
2014-05-10 11:58:47 +0300

    Add bash UTF-8 fixer

---
+++ bash-utf8-fix.sh
@@ -0,0 +1,7 @@
+
+sudo bash -c "echo 'export LANGUAGE=en_US.UTF-8' >> /etc/bash.bashrc"
+sudo bash -c "echo 'export LANG=en_US.UTF-8' >> /etc/bash.bashrc"
+sudo bash -c "echo 'export LC_ALL=en_US.UTF-8' >> /etc/bash.bashrc"
+
+sudo locale-gen en_US.UTF-8
+sudo dpkg-reconfigure locales
